37df4149a816940331a6c6b5a6af8153f16179b4
Athou <jeremiepanzer@gmail.com>
2011-12-15 09:30:48 +0100

    BotCommands are now named in weld

--- src/main/java/be/hehehe/geekbot/annotations/BotCommand.java
+++ src/main/java/be/hehehe/geekbot/annotations/BotCommand.java
@@ -5,6 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.inject.Named;
+
 /**
  * 
  * Annotate any class to indicate that it contains trigger commands.
@@ -12,6 +14,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Named
 public @interface BotCommand {
 
 }

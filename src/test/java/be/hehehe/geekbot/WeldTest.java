ca154cb7a51655d892ac54a4e88e9c9b8a47059c
Athou <jeremiepanzer@gmail.com>
2011-12-15 11:43:06 +0100

    WeldTest should be abstract

--- src/test/java/be/hehehe/geekbot/WeldTest.java
+++ src/test/java/be/hehehe/geekbot/WeldTest.java
@@ -3,7 +3,7 @@ package be.hehehe.geekbot;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 
-public class WeldTest {
+public abstract class WeldTest {
 	private static WeldContainer container = new Weld().initialize();
 
 	protected static <T> T lookup(Class<T> klass) {

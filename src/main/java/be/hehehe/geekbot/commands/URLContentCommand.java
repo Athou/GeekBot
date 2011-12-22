9f6290e42b9ba285825576b7a6d89f21948527ef
Athou <jeremiepanzer@gmail.com>
2011-12-22 14:29:19 +0100

    check for null

--- src/main/java/be/hehehe/geekbot/commands/URLContentCommand.java
+++ src/main/java/be/hehehe/geekbot/commands/URLContentCommand.java
@@ -34,73 +34,75 @@ public class URLContentCommand {
 		List<String> result = new ArrayList<String>();
 
 		String url = utilsService.extractURL(event.getMessage());
-
-		// youtube
-		if (url.contains("youtube.com") || url.contains("youtu.be")) {
-			String videoParam = null;
-			if (url.contains("youtube.com")) {
-				videoParam = utilsService.getRequestParametersFromURL(url).get(
-						"v");
-			} else {
-				videoParam = utilsService.extractIDFromYoutuDotBeURL(url);
-			}
-			String data = "http://gdata.youtube.com/feeds/api/videos/"
-					+ videoParam;
-			try {
-				String content = utilsService.getContent(data);
-				Document doc = utilsService.parseXML(content);
-				Element root = doc.getRootElement();
-				String title = "";
-				for (Object o : root.getChildren()) {
-					Element e = (Element) o;
-					if ("title".equals(e.getName())) {
-						title = e.getText();
-						break;
-					}
+		if (url != null) {
+			// youtube
+			if (url.contains("youtube.com") || url.contains("youtu.be")) {
+				String videoParam = null;
+				if (url.contains("youtube.com")) {
+					videoParam = utilsService.getRequestParametersFromURL(url)
+							.get("v");
+				} else {
+					videoParam = utilsService.extractIDFromYoutuDotBeURL(url);
 				}
-				String line = IRCUtils.bold("Youtube") + " - " + title;
-				result.add(line);
-			} catch (Exception e) {
-				LOG.handle(e);
+				String data = "http://gdata.youtube.com/feeds/api/videos/"
+						+ videoParam;
+				try {
+					String content = utilsService.getContent(data);
+					Document doc = utilsService.parseXML(content);
+					Element root = doc.getRootElement();
+					String title = "";
+					for (Object o : root.getChildren()) {
+						Element e = (Element) o;
+						if ("title".equals(e.getName())) {
+							title = e.getText();
+							break;
+						}
+					}
+					String line = IRCUtils.bold("Youtube") + " - " + title;
+					result.add(line);
+				} catch (Exception e) {
+					LOG.handle(e);
+				}
 			}
-		}
 
-		// vimeo
-		if (url.contains("vimeo.com")) {
-			String videoParam = utilsService.extractIDFromYoutuDotBeURL(url);
-			String data = "http://vimeo.com/api/v2/video/" + videoParam
-					+ ".json";
-			try {
+			// vimeo
+			if (url.contains("vimeo.com")) {
+				String videoParam = utilsService
+						.extractIDFromYoutuDotBeURL(url);
+				String data = "http://vimeo.com/api/v2/video/" + videoParam
+						+ ".json";
+				try {
+					String content = utilsService.getContent(data);
+					JSONArray array = new JSONArray(content);
+					String title = array.getJSONObject(0).getString("title");
+					String line = IRCUtils.bold("Vimeo") + " - " + title;
+					result.add(line);
+				} catch (Exception e) {
+					LOG.handle(e);
+				}
+			}
+
+			// twitter
+
+			if (url.contains("twitter.com") && url.contains("/status/")) {
+				String statusId = utilsService.extractIDFromYoutuDotBeURL(url);
+				String data = "https://api.twitter.com/1/statuses/show.json?id="
+						+ statusId + "&include_entities=true";
 				String content = utilsService.getContent(data);
-				JSONArray array = new JSONArray(content);
-				String title = array.getJSONObject(0).getString("title");
-				String line = IRCUtils.bold("Vimeo") + " - " + title;
+				String name = null;
+				String text = null;
+
+				try {
+					JSONObject root = new JSONObject(content);
+					name = root.getJSONObject("user").getString("name");
+					text = root.getString("text");
+				} catch (Exception e) {
+					LOG.handle(e);
+				}
+				String line = IRCUtils.bold(name + ": ") + text;
 				result.add(line);
-			} catch (Exception e) {
-				LOG.handle(e);
+
 			}
-		}
-
-		// twitter
-
-		if (url.contains("twitter.com") && url.contains("/status/")) {
-			String statusId = utilsService.extractIDFromYoutuDotBeURL(url);
-			String data = "https://api.twitter.com/1/statuses/show.json?id="
-					+ statusId + "&include_entities=true";
-			String content = utilsService.getContent(data);
-			String name = null;
-			String text = null;
-
-			try {
-				JSONObject root = new JSONObject(content);
-				name = root.getJSONObject("user").getString("name");
-				text = root.getString("text");
-			} catch (Exception e) {
-				LOG.handle(e);
-			}
-			String line = IRCUtils.bold(name + ": ") + text;
-			result.add(line);
-
 		}
 		return result;
 	}

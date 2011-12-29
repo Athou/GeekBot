a8a9daaaf524a8be160e3eb4d4b04a2e08fceb37
Athou <jeremiepanzer@gmail.com>
2011-12-29 10:36:35 +0100

    fixing rot13 command

--- src/main/java/be/hehehe/geekbot/commands/Rot13Command.java
+++ src/main/java/be/hehehe/geekbot/commands/Rot13Command.java
@@ -2,25 +2,28 @@ package be.hehehe.geekbot.commands;
 
 import be.hehehe.geekbot.annotations.BotCommand;
 import be.hehehe.geekbot.annotations.Trigger;
-import be.hehehe.geekbot.utils.LOG;
+import be.hehehe.geekbot.annotations.TriggerType;
+import be.hehehe.geekbot.bot.TriggerEvent;
 
 @BotCommand
 public class Rot13Command {
 
 	@Trigger(value = "!rot13", type = TriggerType.STARTSWITH)
 	public String update(TriggerEvent event) {
-		String message = "";
-		
-		String text = event.getMessage();
-		for (int i = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            if       (c >= 'a' && c <= 'm') c += 13;
-            else if  (c >= 'n' && c <= 'z') c -= 13;
-            else if  (c >= 'A' && c <= 'M') c += 13;
-            else if  (c >= 'A' && c <= 'Z') c -= 13;
-            message += c;
-        }
-		
-		return message;
+		StringBuilder message = new StringBuilder();
+
+		for (char c : event.getMessage().toCharArray()) {
+			if (c >= 'a' && c <= 'm')
+				c += 13;
+			else if (c >= 'n' && c <= 'z')
+				c -= 13;
+			else if (c >= 'A' && c <= 'M')
+				c += 13;
+			else if (c >= 'A' && c <= 'Z')
+				c -= 13;
+			message.append(c);
+		}
+
+		return message.toString();
 	}
 }

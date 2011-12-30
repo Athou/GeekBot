894b4fd85ac0b62eacfcb050b9772423b41650cd
Athou <jeremiepanzer@gmail.com>
2011-12-30 11:17:29 +0100

    cleaning up regex

--- src/main/java/be/hehehe/geekbot/utils/BotUtilsService.java
+++ src/main/java/be/hehehe/geekbot/utils/BotUtilsService.java
@@ -124,7 +124,7 @@ public class BotUtilsService {
 	 */
 	public String extractURL(String message) {
 		String url = null;
-		for (String s : message.split("[ ]")) {
+		for (String s : message.split(" ")) {
 			if (s.contains("http://") || s.contains("https://")
 					|| s.contains("www.")) {
 				if (s.endsWith("/")) {

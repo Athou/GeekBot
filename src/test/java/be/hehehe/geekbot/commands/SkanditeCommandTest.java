7b11040eba19792a854d46b6370a98e7de5bbf2a
Athou <jeremiepanzer@gmail.com>
2012-01-12 10:24:35 +0100

    changing the test and adding a mock bot utils so that it will execute faster

--- src/test/java/be/hehehe/geekbot/commands/SkanditeCommandTest.java
+++ src/test/java/be/hehehe/geekbot/commands/SkanditeCommandTest.java
@@ -1,16 +1,20 @@
 package be.hehehe.geekbot.commands;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import be.hehehe.geekbot.WeldRunner;
 import be.hehehe.geekbot.bot.TriggerEvent;
 import be.hehehe.geekbot.bot.TriggerEventImpl;
+import be.hehehe.geekbot.utils.BotUtilsService;
+import be.hehehe.geekbot.utils.HashAndByteCount;
 
 @RunWith(WeldRunner.class)
 public class SkanditeCommandTest {
@@ -18,10 +22,28 @@ public class SkanditeCommandTest {
 	@Inject
 	SkanditeCommand skanditeCommand;
 
+	@Before
+	public void init() {
+		skanditeCommand.utilsService = new BotUtilsService() {
+			@Override
+			public HashAndByteCount calculateHashAndByteCount(String urlString) {
+				HashAndByteCount hashAndByteCount = new HashAndByteCount();
+				hashAndByteCount.setByteCount((long) urlString.length());
+				hashAndByteCount.setHash(urlString);
+				return hashAndByteCount;
+			}
+
+			@Override
+			public String getTimeDifference(Date pastDate) {
+				return "dummy";
+			}
+		};
+	}
+
 	@Test
 	public void skanditeTest() {
-		TriggerEvent event = new TriggerEventImpl("http://www.commabeat.com",
-				"http://www.commabeat.com");
+		TriggerEvent event = new TriggerEventImpl("http://www.google.com",
+				"http://www.google.com");
 		List<String> result = skanditeCommand.handleSkandites(event);
 		Assert.assertTrue(result.isEmpty());
 		result = skanditeCommand.handleSkandites(event);

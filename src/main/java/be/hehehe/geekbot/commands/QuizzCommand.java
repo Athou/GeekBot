682a94bf31f9cbd345604cd7baf3dae176da6df8
Athou <jeremiepanzer@gmail.com>
2012-04-12 09:08:42 +0200

    Quizz !

---
+++ src/main/java/be/hehehe/geekbot/commands/QuizzCommand.java
@@ -0,0 +1,154 @@
+package be.hehehe.geekbot.commands;
+
+import java.text.Normalizer;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+import be.hehehe.geekbot.annotations.BotCommand;
+import be.hehehe.geekbot.annotations.Help;
+import be.hehehe.geekbot.annotations.Trigger;
+import be.hehehe.geekbot.annotations.TriggerType;
+import be.hehehe.geekbot.bot.State;
+import be.hehehe.geekbot.bot.TriggerEvent;
+import be.hehehe.geekbot.utils.IRCUtils;
+
+@BotCommand
+public class QuizzCommand {
+
+	@Inject
+	State state;
+
+	@Inject
+	Logger log;
+
+	private static final String ENABLED = "enabled";
+	private static final String CURRENT_QUESTION = "current-question";
+	private static final String CURRENT_ANSWER = "current-answer";
+	private static final String CURRENT_TIMER = "current-timer";
+
+	@Trigger(value = "!quizz")
+	@Help("Starts the quizz.")
+	public void startQuizz(TriggerEvent event) {
+		if (Boolean.TRUE.equals(state.get(ENABLED))) {
+			event.write("The quizz has already started.");
+		} else {
+			state.put(ENABLED, Boolean.TRUE);
+			nextQuestion(event);
+		}
+	}
+
+	@Trigger(value = "!stop")
+	@Help("Stops the quizz.")
+	public void stopQuizz(TriggerEvent event) {
+		state.put(ENABLED, Boolean.FALSE);
+		event.write("Quizz stopped.");
+	}
+
+	@Trigger(value = "!tg")
+	@Help("Stops the quizz.")
+	public void stopQuizz2(TriggerEvent event) {
+		stopQuizz(event);
+	}
+
+	private void nextQuestion(TriggerEvent event) {
+		try {
+			List<String> lines = IOUtils.readLines(getClass()
+					.getResourceAsStream("/quizz.txt"));
+			int rand = new Random().nextInt(lines.size());
+
+			String line = lines.get(rand);
+			String[] split = line.split("\\\\");
+
+			String question = split[0].trim();
+			String answer = split[1].trim();
+
+			state.put(CURRENT_QUESTION, question);
+			state.put(CURRENT_ANSWER, answer);
+
+			event.write(IRCUtils.bold("Question! ") + question);
+
+			startTimeoutTimer(event);
+
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+
+	}
+
+	private void startTimeoutTimer(final TriggerEvent event) {
+		ScheduledExecutorService scheduler = Executors
+				.newScheduledThreadPool(1);
+		Runnable thread = new Runnable() {
+			@Override
+			public void run() {
+				event.write(IRCUtils.bold("Trop tard!") + "La réponse était: "
+						+ state.get(CURRENT_ANSWER));
+			}
+		};
+		ScheduledFuture<?> timer = scheduler.schedule(thread, 20,
+				TimeUnit.SECONDS);
+		state.put(CURRENT_TIMER, timer);
+	}
+
+	@Trigger(type = TriggerType.EVERYTHING)
+	public String handlePossibleAnswer(TriggerEvent event) {
+		String result = null;
+		String answer = state.get(CURRENT_ANSWER, String.class);
+		if (Boolean.TRUE.equals(state.get(ENABLED))
+				&& !event.isStartsWithTrigger()) {
+			if (matches(event.getMessage(), answer)) {
+				event.write(IRCUtils.bold("Bien joué " + event.getAuthor()
+						+ "! ")
+						+ "La réponse était: " + answer);
+
+				// cancel timer
+				ScheduledFuture<?> timer = state.get(CURRENT_TIMER,
+						ScheduledFuture.class);
+				if (timer != null) {
+					timer.cancel(true);
+				}
+
+				nextQuestion(event);
+			}
+		}
+		return result;
+	}
+
+	public boolean matches(String proposition, String answer) {
+		boolean match = false;
+
+		proposition = StringUtils.trimToEmpty(stripAccents(proposition))
+				.toUpperCase();
+		answer = StringUtils.trimToEmpty(stripAccents(answer)).toUpperCase();
+
+		// check if those are numbers
+		try {
+			double p = Double.parseDouble(proposition.replace(" ", ""));
+			double a = Double.parseDouble(answer.replace(" ", ""));
+			match |= (p == a);
+		} catch (Exception e) {
+			// do nothing
+		}
+
+		// be permissive regarding the response (experimental)
+		match |= (StringUtils.getLevenshteinDistance(proposition, answer) <= 1);
+
+		return match;
+
+	}
+
+	private String stripAccents(String source) {
+		source = Normalizer.normalize(source, Normalizer.Form.NFD);
+		return source.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+	}
+}
---
+++ src/test/java/be/hehehe/geekbot/commands/QuizzCommandTest.java
@@ -0,0 +1,32 @@
+package be.hehehe.geekbot.commands;
+
+import javax.inject.Inject;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import be.hehehe.geekbot.WeldRunner;
+
+@RunWith(WeldRunner.class)
+public class QuizzCommandTest {
+
+	@Inject
+	QuizzCommand quizzCommand;
+
+	@Test
+	public void matchIntegersTest() {
+		Assert.assertTrue(quizzCommand.matches("1000", "1 000"));
+	}
+	
+	@Test
+	public void matchStrippedAccentsTest() {
+		Assert.assertTrue(quizzCommand.matches("garcon", "garçon"));
+		Assert.assertTrue(quizzCommand.matches("education", "éducation"));
+	}
+	
+	@Test
+	public void matchAlmostTest() {
+		Assert.assertTrue(quizzCommand.matches("paquebo", "Paquebot"));
+	}
+}

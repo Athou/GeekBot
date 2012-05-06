5561d20cc1e07c40bbb53089ccf687e01db4abd3
Athou <jeremiepanzer@gmail.com>
2012-05-06 14:05:06 +0200

    cleanup

--- src/main/java/be/hehehe/geekbot/web/server/QuizzServiceImpl.java
+++ src/main/java/be/hehehe/geekbot/web/server/QuizzServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.lang.StringUtils;
 
 import be.hehehe.geekbot.annotations.GWTServlet;
@@ -16,26 +15,12 @@ import be.hehehe.geekbot.persistence.model.QuizzPlayer;
 import be.hehehe.geekbot.utils.BundleService;
 import be.hehehe.geekbot.web.client.QuizzService;
 
-import com.google.common.collect.Lists;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 @SuppressWarnings("serial")
 @GWTServlet(path = "/Quizz/quizz")
 public class QuizzServiceImpl extends RemoteServiceServlet implements
 		QuizzService {
-
-	@SuppressWarnings("unchecked")
-	public <T> List<T> copy(List<T> list) {
-		List<T> newList = Lists.newArrayList();
-		try {
-			for (T t : list) {
-				newList.add((T) BeanUtils.cloneBean(t));
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		return newList;
-	}
 
 	@Inject
 	QuizzDAO quizzDAO;
@@ -48,13 +33,13 @@ public class QuizzServiceImpl extends RemoteServiceServlet implements
 
 	@Override
 	public List<QuizzPlayer> getPlayers() {
-		return copy(quizzDAO.getPlayersOrderByPoints());
+		return quizzDAO.getPlayersOrderByPoints();
 
 	}
 
 	@Override
 	public List<QuizzMergeRequest> getRequests() {
-		return copy(quizzMergeDAO.findAll());
+		return quizzMergeDAO.findAll();
 	}
 
 	@Override

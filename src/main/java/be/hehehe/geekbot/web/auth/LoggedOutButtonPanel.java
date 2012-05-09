60dce292a700b78d1dfd03fd1896cd2399e69ac9
Athou <jeremiepanzer@gmail.com>
2012-05-09 22:07:52 +0200

    use a BookmarkablePageLink where possible

--- src/main/java/be/hehehe/geekbot/web/auth/LoggedOutButtonPanel.java
+++ src/main/java/be/hehehe/geekbot/web/auth/LoggedOutButtonPanel.java
@@ -1,6 +1,6 @@
 package be.hehehe.geekbot.web.auth;
 
-import org.apache.wicket.markup.html.link.StatelessLink;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
 
 @SuppressWarnings("serial")
@@ -8,13 +8,6 @@ public class LoggedOutButtonPanel extends Panel {
 
 	public LoggedOutButtonPanel(String id) {
 		super(id);
-		add(new StatelessLink<String>("login") {
-			@Override
-			public void onClick() {
-				setResponsePage(LoginPage.class);
-			}
-		});
-
+		add(new BookmarkablePageLink<String>("login", LoginPage.class));
 	}
-
 }

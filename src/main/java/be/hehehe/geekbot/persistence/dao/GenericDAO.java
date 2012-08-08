55e8b9973de8df7d0335103f501d4e280927ebba
Jeremie Panzer <jeremie.panzer@erasme.ulb.ac.be>
2012-08-08 14:54:34 +0200

    pu name

--- src/main/java/be/hehehe/geekbot/persistence/dao/GenericDAO.java
+++ src/main/java/be/hehehe/geekbot/persistence/dao/GenericDAO.java
@@ -12,7 +12,7 @@ import javax.persistence.criteria.Root;
 
 public abstract class GenericDAO<T> {
 
-	@PersistenceContext(unitName = "geekbotPU")
+	@PersistenceContext
 	protected EntityManager em;
 
 	protected CriteriaBuilder builder;

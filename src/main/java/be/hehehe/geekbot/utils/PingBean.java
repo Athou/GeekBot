8deb6a7d5983acb3b741414229b92ee18eb5d9e3
Athou <jeremiepanzer@gmail.com>
2012-08-18 12:34:23 +0200

    don't start pingbean at startup

--- src/main/java/be/hehehe/geekbot/utils/PingBean.java
+++ src/main/java/be/hehehe/geekbot/utils/PingBean.java
@@ -3,16 +3,13 @@ package be.hehehe.geekbot.utils;
 import java.io.InputStream;
 import java.net.URL;
 
-import javax.annotation.PostConstruct;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
-import javax.ejb.Startup;
 import javax.inject.Inject;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 
-@Startup
 @Singleton
 public class PingBean {
 
@@ -23,7 +20,6 @@ public class PingBean {
 	Logger log;
 
 	@Schedule(hour = "*", persistent = false)
-	@PostConstruct
 	private void refresh() {
 		InputStream stream = null;
 		try {

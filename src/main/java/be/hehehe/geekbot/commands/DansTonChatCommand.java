b249ea0ac9bdc8f84bb463e8b87024a140748746
Jeremie Panzer <jeremie.panzer@erasme.ulb.ac.be>
2012-10-26 11:38:53 +0200

    added DTC

---
+++ src/main/java/be/hehehe/geekbot/commands/DansTonChatCommand.java
@@ -0,0 +1,60 @@
+package be.hehehe.geekbot.commands;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.log4j.Logger;
+import org.jdom2.Document;
+import org.jdom2.Element;
+
+import be.hehehe.geekbot.annotations.BotCommand;
+import be.hehehe.geekbot.annotations.Trigger;
+import be.hehehe.geekbot.utils.BotUtilsService;
+import be.hehehe.geekbot.utils.IRCUtils;
+
+import com.google.common.collect.Lists;
+
+@BotCommand
+public class DansTonChatCommand {
+
+	// key from android apk
+	private static final String API_URL = "http://api.danstonchat.com/0.2/view/random0?key=cad048f021e9413b";
+
+	@Inject
+	BotUtilsService utilsService;
+
+	@Inject
+	Logger log;
+
+	@Trigger("!dtc")
+	public List<String> getRandom() {
+		List<String> list = Lists.newArrayList();
+		String xml = utilsService.getContent(API_URL);
+		try {
+			Document doc = utilsService.parseXML(xml);
+			Element item = null;
+			for (Element element : doc.getRootElement().getChildren("item")) {
+				String content = element.getChildText("content");
+				if (content.split(SystemUtils.LINE_SEPARATOR).length <= 5) {
+					item = element;
+					break;
+				}
+			}
+			String content = item.getChildText("content");
+			String upvote = item.getChildText("vote_plus");
+			String downvote = item.getChildText("vote_minus");
+			for (String line : content.split(SystemUtils.LINE_SEPARATOR)) {
+				list.add(line);
+			}
+			list.add(IRCUtils.bold("LOL")
+					+ String.format(" (+%s/-%s)", upvote, downvote));
+		} catch (Exception e) {
+			list.add("Could not contact DTC.");
+			log.error(e.getMessage(), e);
+		}
+		return list;
+	}
+
+}

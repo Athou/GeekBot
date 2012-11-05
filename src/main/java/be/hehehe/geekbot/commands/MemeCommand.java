d4ceb46c557600af231463327031599ba95a9aa2
Jeremie Panzer <jeremie.panzer@erasme.ulb.ac.be>
2012-11-05 09:13:17 +0100

    remove partially hardcoded sentences, and use popular generators in addition to hardcoded ones

--- src/main/java/be/hehehe/geekbot/commands/MemeCommand.java
+++ src/main/java/be/hehehe/geekbot/commands/MemeCommand.java
@@ -1,27 +1,34 @@
 package be.hehehe.geekbot.commands;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.apache.log4j.Logger;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import be.hehehe.geekbot.annotations.BotCommand;
+import be.hehehe.geekbot.annotations.TimedAction;
 import be.hehehe.geekbot.annotations.Trigger;
 import be.hehehe.geekbot.persistence.dao.ConnerieDAO;
 import be.hehehe.geekbot.utils.BotUtilsService;
 import be.hehehe.geekbot.utils.BundleService;
+import be.hehehe.geekbot.utils.IRCUtils;
 
 import com.google.common.collect.Lists;
 
 @BotCommand
 public class MemeCommand {
 
-	private static final String URL = "http://version1.api.memegenerator.net/Instance_Create?username=%s&password=%s&languageCode=en&generatorID=%d&imageID=%d&text0=%s&text1=%s";
+	private static final String CREATE_URL = "http://version1.api.memegenerator.net/Instance_Create?username=%s&password=%s&languageCode=en&generatorID=%s&imageID=%s&text0=%s&text1=%s";
+	private static final String POPULAR_GENERATORS = "http://version1.api.memegenerator.net/Generators_Select_ByPopular?pageIndex=0&pageSize=24&days=7";
 
 	@Inject
 	Logger log;
@@ -38,29 +45,65 @@ public class MemeCommand {
 	@Inject
 	BotUtilsService utilsService;
 
-	private List<Phrase> phrases = Lists.newArrayList();
+	private List<Generator> generators = Lists.newCopyOnWriteArrayList();
 
 	@PostConstruct
-	public void init() {
-		phrases.add(new Phrase("%s", "Y U NO %s", true, true, 2, 166088));
-		phrases.add(new Phrase("I DON'T ALWAYS %s", "BUT WHEN I DO, %s", true,
-				true, 74, 2485));
-		phrases.add(new Phrase("%s", "O RLY?", true, false, 920, 117049));
-		phrases.add(new Phrase("%s", "ALL THE %s", true, true, 6013, 1121885));
-		phrases.add(new Phrase("GOOD NEWS EVERYONE", "%s", false, true, 1591,
-				112464));
-		phrases.add(new Phrase("NOT SURE IF %s", "OR %s", true, true, 305,
-				84688));
-		phrases.add(new Phrase("YO DAWG %s", "SO %s", true, true, 79, 108785));
-		phrases.add(new Phrase("ALL YOUR %s", "ARE BELONG TO US", true, false,
-				349058, 2079825));
-		phrases.add(new Phrase("%s", "FUCK YOU", true, false, 1189472, 5044147));
-		phrases.add(new Phrase("%s", "You're gonna have a bad time", true,
-				false, 825296, 3786537));
-		phrases.add(new Phrase("one does not simply", "%s", false, true,
-				274947, 1865027));
-		phrases.add(new Phrase("%s", "NAILED IT", true, false, 121, 1031));
-		phrases.add(new Phrase("%s", "TOO DAMN %s", true, true, 998, 203665));
+	private void init() {
+		// y u no
+		generators.add(new Generator("2", "166088"));
+		// most interesting man on earth
+		generators.add(new Generator("74", "2485"));
+		// o'rly
+		generators.add(new Generator("920", "117049"));
+		// * all the things
+		generators.add(new Generator("6013", "1121885"));
+		// good news everyone
+		generators.add(new Generator("1591", "112464"));
+		// fry not sure if
+		generators.add(new Generator("305", "84688"));
+		// yo dawg
+		generators.add(new Generator("79", "108785"));
+		// all your base are belong to us
+		generators.add(new Generator("349058", "2079825"));
+		// fuck you
+		generators.add(new Generator("1189472", "5044147"));
+		// You're gonna have a bad time
+		generators.add(new Generator("825296", "3786537"));
+		// one does not simply
+		generators.add(new Generator("274947", "1865027"));
+		// nailed it
+		generators.add(new Generator("121", "1031"));
+		// to damn
+		generators.add(new Generator("998", "203665"));
+
+		addPopular();
+	}
+
+	private void addPopular() {
+		try {
+			String content = utilsService.getContent(POPULAR_GENERATORS);
+			JSONObject json = new JSONObject(content);
+			JSONArray results = json.getJSONArray("result");
+			for (int i = 0; i < results.length(); i++) {
+				JSONObject result = results.getJSONObject(i);
+				String generatorId = result.getString("generatorID");
+
+				String imageUrl = result.getString("imageUrl");
+				String[] tokens = imageUrl.split(Pattern.quote("/"));
+				String imageName = tokens[tokens.length - 1];
+				String imageId = imageName.split(Pattern.quote("."))[0];
+
+				generators.add(new Generator(generatorId, imageId));
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	@TimedAction(value = 1, timeUnit = TimeUnit.HOURS)
+	private void refreshGenerators() {
+		generators.clear();
+		init();
 	}
 
 	@Trigger("!meme")
@@ -68,29 +111,20 @@ public class MemeCommand {
 		String result = null;
 
 		try {
-			int index = random.nextInt(phrases.size());
-			Phrase phrase = phrases.get(index);
+			int index = random.nextInt(generators.size());
+			Generator generator = generators.get(index);
 
-			String text0 = phrase.getText0();
-			if (phrase.isReplace0()) {
-				text0 = String.format(text0, getRandomString());
-			}
-			text0 = URLEncoder.encode(text0, "UTF-8");
+			String text0 = getRandomText();
+			String text1 = getRandomText();
 
-			String text1 = phrase.getText1();
-			if (phrase.isReplace1()) {
-				text1 = String.format(text1, getRandomString());
-			}
-			text1 = URLEncoder.encode(text1, "UTF-8");
-
-			int generatorId = phrase.getGeneratorId();
-			int imageId = phrase.getImageId();
+			String generatorId = generator.getGeneratorId();
+			String imageId = generator.getImageId();
 
 			String login = bundle.getMemeGeneratorLogin();
 			String password = bundle.getMemeGeneratorPassword();
 
-			String url = String.format(URL, login, password, generatorId,
-					imageId, text0, text1);
+			String url = String.format(CREATE_URL, login, password,
+					generatorId, imageId, text0, text1);
 
 			String content = utilsService.getContent(url);
 
@@ -103,54 +137,33 @@ public class MemeCommand {
 			log.error(e.getMessage(), e);
 			result = e.getMessage();
 		}
-		return result;
+		return IRCUtils.bold("10blague! ") + result;
 	}
 
-	private String getRandomString() {
-		return utilsService.stripAccents(dao.getRandom().getValue());
+	private String getRandomText() {
+		String text = utilsService.stripAccents(dao.getRandom().getValue());
+		try {
+			text = URLEncoder.encode(text, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// do nothing
+		}
+		return text;
 	}
 
-	private class Phrase {
-		private String text0;
-		private String text1;
-		private boolean replace0;
-		private boolean replace1;
-		private int generatorId;
-		private int imageId;
+	private class Generator {
+		private String generatorId;
+		private String imageId;
 
-		public Phrase(String text0, String text1, boolean replace0,
-				boolean replace1, int generatorId, int imageId) {
-			this.text0 = text0;
-			this.text1 = text1;
-
-			this.replace0 = replace0;
-			this.replace1 = replace1;
-
+		public Generator(String generatorId, String imageId) {
 			this.generatorId = generatorId;
 			this.imageId = imageId;
 		}
 
-		public String getText0() {
-			return text0;
-		}
-
-		public String getText1() {
-			return text1;
-		}
-
-		public boolean isReplace0() {
-			return replace0;
-		}
-
-		public boolean isReplace1() {
-			return replace1;
-		}
-
-		public int getGeneratorId() {
+		public String getGeneratorId() {
 			return generatorId;
 		}
 
-		public int getImageId() {
+		public String getImageId() {
 			return imageId;
 		}
 

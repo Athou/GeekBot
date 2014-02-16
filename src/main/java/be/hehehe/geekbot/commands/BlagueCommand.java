09dcac3b05dc6c31a378548ad7fd9caa71e2642b
Athou <jeremiepanzer@gmail.com>
2014-02-16 15:07:02 +0100

    prevent infinite loops

--- src/main/java/be/hehehe/geekbot/commands/BlagueCommand.java
+++ src/main/java/be/hehehe/geekbot/commands/BlagueCommand.java
@@ -34,9 +34,9 @@ public class BlagueCommand {
 	@Trigger("!blague")
 	@Help("Fetches a random blague from la-blague-du-jour.com")
 	public List<String> getRandomBlague() {
-		List<String> toReturn = new ArrayList<String>();
+		List<String> result = new ArrayList<String>();
+		String url = "http://www.la-blague-du-jour.com/blagues_au_hasard/Une_blague_aleatoire.html";
 		try {
-			String url = "http://www.la-blague-du-jour.com/blagues_au_hasard/Une_blague_aleatoire.html";
 			Document doc = Jsoup.parse(utilsService.getContent(url));
 
 			String cat = doc.select("p.Paratexte b a").text();
@@ -48,21 +48,21 @@ public class BlagueCommand {
 			String blague = doc.select("p.TexteBlague").html();
 			List<String> lines = Arrays.asList(blague.split("<br />"));
 			for (String line : lines) {
-				toReturn.add(StringEscapeUtils.unescapeHtml4(line));
+				result.add(StringEscapeUtils.unescapeHtml4(line));
 			}
 
-			if (toReturn.size() > 5) {
+			if (result.size() > 5) {
 				// joke too long
 				return getRandomBlague();
 			}
 
-			toReturn.add(0, IRCUtils.bold("Mega Vanne") + " - " + cat);
-			toReturn.add(IRCUtils.bold("http://www.instantrimshot.com/"));
+			result.add(0, IRCUtils.bold("Mega Vanne") + " - " + cat);
+			result.add(IRCUtils.bold("http://www.instantrimshot.com/"));
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
-			return getRandomBlague();
+			result.add("Could not contact " + url);
 		}
 
-		return toReturn;
+		return result;
 	}
 }
